f9451c234c0e6621f57ca1a288a7cf1929b69219
Aureus, Carlo <carlo_aureus@intuit.com>
2021-06-26 22:03:17 -0700

    add transform method for ResolverArgumentDefinition

--- src/main/java/com/intuit/graphql/orchestrator/resolverdirective/ResolverArgumentDefinition.java
+++ src/main/java/com/intuit/graphql/orchestrator/resolverdirective/ResolverArgumentDefinition.java
@@ -3,15 +3,52 @@ package com.intuit.graphql.orchestrator.resolverdirective;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.util.function.Consumer;
+
 @Getter
 @ToString
 public class ResolverArgumentDefinition {
 
-  private String name;
-  private String value;
+  private final String name;
+  private final String value;
 
   public ResolverArgumentDefinition(String name, String value) {
     this.name = name;
     this.value = value;
   }
+
+  public ResolverArgumentDefinition transform(Consumer<ResolverArgumentDefinition.Builder> consumer) {
+    ResolverArgumentDefinition.Builder builder = new ResolverArgumentDefinition.Builder(this);
+    consumer.accept(builder);
+    return builder.build();
+  }
+
+  public static class Builder {
+
+    private String name;
+    private String value;
+
+    public Builder() {
+    }
+
+    public Builder(ResolverArgumentDefinition copy) {
+      this.name = copy.getName();
+      this.value = copy.getValue();
+    }
+
+    public ResolverArgumentDefinition.Builder name(String name) {
+      this.name = name;
+      return this;
+    }
+
+    public ResolverArgumentDefinition.Builder value(String value) {
+      this.value = value;
+      return this;
+    }
+
+    public ResolverArgumentDefinition build() {
+      return new ResolverArgumentDefinition(name, value);
+    }
+  }
+
 }
