544db40e4821e3f3fc4504e1f081c300d2209842
Aureus, Carlo <carlo_aureus@intuit.com>
2021-10-22 11:31:55 -0700

    using @Getter annotation for remainingSelectionsCount

--- src/main/java/com/intuit/graphql/orchestrator/authorization/SelectionSetMetadata.java
+++ src/main/java/com/intuit/graphql/orchestrator/authorization/SelectionSetMetadata.java
@@ -6,15 +6,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public class SelectionSetMetadata {
 
-    private int remainingSelectionsCount;
+    @Getter private int remainingSelectionsCount;
     @Getter private final String selectionSetPath;
 
     public void decreaseRemainingSelection() {
         --this.remainingSelectionsCount;
-    }
-
-    public int getRemainingSelectionsCount() {
-        return this.remainingSelectionsCount;
     }
 
 }
