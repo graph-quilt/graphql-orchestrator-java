53efd1760862e812a7f3ae7be4c205c2d546141b
Aureus, Carlo <carlo_aureus@intuit.com>
2022-02-03 15:47:26 -0800

    initial commit

--- src/main/java/com/intuit/graphql/orchestrator/batch/NoExternalReferenceSelectionSetModifier.java
+++ src/main/java/com/intuit/graphql/orchestrator/batch/NoExternalReferenceSelectionSetModifier.java
@@ -2,6 +2,7 @@ package com.intuit.graphql.orchestrator.batch;
 
 import static com.intuit.graphql.orchestrator.resolverdirective.FieldResolverDirectiveUtil.hasResolverDirective;
 import static graphql.introspection.Introspection.TypeNameMetaFieldDef;
+import static graphql.util.TreeTransformerUtil.changeNode;
 import static graphql.util.TreeTransformerUtil.deleteNode;
 import static java.util.Objects.requireNonNull;
 
@@ -11,12 +12,16 @@ import graphql.language.InlineFragment;
 import graphql.language.Node;
 import graphql.language.NodeVisitorStub;
 import graphql.language.SelectionSet;
+import graphql.schema.GraphQLArgument;
 import graphql.schema.GraphQLFieldDefinition;
 import graphql.schema.GraphQLFieldsContainer;
 import graphql.schema.GraphQLType;
 import graphql.schema.GraphQLTypeUtil;
 import graphql.util.TraversalControl;
 import graphql.util.TraverserContext;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.apache.commons.collections4.CollectionUtils;
 
 public class NoExternalReferenceSelectionSetModifier extends NodeVisitorStub {
 
@@ -70,11 +75,44 @@ public class NoExternalReferenceSelectionSetModifier extends NodeVisitorStub {
     return TraversalControl.CONTINUE;
   }
 
+  // Tests needed
+  // Queries that has field with resolver selected in a Selection as (Field),
+  // in FragmentSpread and Inline Fragment
+  //
   @Override
   public TraversalControl visitSelectionSet(SelectionSet node, TraverserContext<Node> context) {
     GraphQLType parentType = getParentType(context);
     context.setVar(GraphQLType.class, parentType);
-    return TraversalControl.CONTINUE;
+
+    List<GraphQLFieldDefinition> fieldsWithResolver = node.getSelections().stream()
+        .filter(selection -> selection instanceof Field)
+        // A selection can be a Field, FragmentSpread and InlineFragment
+        // For InlineFragment/FragmentSpread just continue since it will have another selection.
+        .map(selection -> (Field) selection)
+        .filter(field -> {
+          GraphQLFieldDefinition fieldDefinition = getFieldDefinition(field.getName(), (GraphQLFieldsContainer) parentType); // not always Field
+          return hasResolverDirective(fieldDefinition);
+        })
+        .map(field -> getFieldDefinition(field.getName(), (GraphQLFieldsContainer) parentType))
+        .collect(Collectors.toList());
+
+    List<GraphQLArgument> resolverDirectiveArguments = fieldsWithResolver.stream()
+        .flatMap(fieldDefinition -> fieldDefinition.getDirectives().stream())
+        .flatMap(graphQLDirective -> graphQLDirective.getArguments().stream())
+        .collect(Collectors.toList());
+
+
+    // TODO get the required fields from resolverDirectiveArguments and check if it exists in selectionSet
+    if (CollectionUtils.isNotEmpty(fieldsWithResolver)) {
+      SelectionSet newSelectSet = node.transform(builder -> {
+        Field newField = Field.newField("petId").build(); // just for test
+        builder.selection(newField);
+        //builder.selection(newField);
+      });
+      return changeNode(context, newSelectSet);
+    } else {
+      return TraversalControl.CONTINUE;
+    }
   }
 
   private GraphQLType getParentType(TraverserContext<Node> context) {
