dbecafffd041b163ef9a015dcd7b1cde996e6abd
kmoore15 <kyle_moore@intuit.com>
2022-02-14 13:17:03 -0500

    Federation entity collection

--- src/main/java/com/intuit/graphql/orchestrator/schema/transform/KeyTransformer.java
+++ src/main/java/com/intuit/graphql/orchestrator/schema/transform/KeyTransformer.java
@@ -3,11 +3,15 @@ package com.intuit.graphql.orchestrator.schema.transform;
 import com.intuit.graphql.graphQL.Argument;
 import com.intuit.graphql.graphQL.Directive;
 import com.intuit.graphql.graphQL.TypeDefinition;
+import com.intuit.graphql.orchestrator.ServiceProvider;
 import com.intuit.graphql.orchestrator.federation.keydirective.KeyDirectiveValidator;
+import com.intuit.graphql.orchestrator.federation.keydirective.exceptions.InvalidLocationForFederationDirective;
 import com.intuit.graphql.orchestrator.xtext.XtextGraph;
 import graphql.VisibleForTesting;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static com.intuit.graphql.orchestrator.utils.XtextUtils.FEDERATION_KEY_DIRECTIVE;
@@ -24,20 +28,24 @@ public class KeyTransformer implements Transformer<XtextGraph, XtextGraph> {
 
   @Override
   public XtextGraph transform(final XtextGraph source) {
-    List<TypeDefinition> entities = source.getTypes().values().stream()
+    Map<String, TypeDefinition> entities = source.getTypes().values().stream()
             .filter(typeDefinition -> typeContainsDirective(typeDefinition, FEDERATION_KEY_DIRECTIVE))
-            .collect(Collectors.toList());
+            .collect(Collectors.toMap(TypeDefinition::getName, Function.identity()));
 
-    for (final TypeDefinition typeDefinition : entities) {
-      for (final Directive directive : typeDefinition.getDirectives()) {
+    if(entities.size() > 0 && source.getServiceProvider().getSeviceType() != ServiceProvider.ServiceType.FEDERATION_SUBGRAPH) {
+      throw new InvalidLocationForFederationDirective(FEDERATION_KEY_DIRECTIVE);
+    }
+
+    for(final TypeDefinition entityDefinitions : entities.values()) {
+      for (final Directive directive : entityDefinitions.getDirectives()) {
         if(directive.getDefinition().getName().equals(FEDERATION_KEY_DIRECTIVE)) {
           List<Argument> arguments = directive.getArguments();
-          keyDirectiveValidator.validate(typeDefinition, arguments);
+          keyDirectiveValidator.validate(entityDefinitions, arguments);
         }
       }
     }
 
-    return source;
+    return source.transform(builder -> builder.entities(entities));
   }
 
 
