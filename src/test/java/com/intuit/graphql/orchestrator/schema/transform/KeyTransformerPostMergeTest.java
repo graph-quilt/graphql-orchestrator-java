1636bcb8ae0fc2a8f1837252297a5df8d4ffe1d6
Aureus, Carlo <carlo_aureus@intuit.com>
2022-02-24 03:12:35 -0800

    KeyTransformerPostMerge test file

---
+++ src/test/java/com/intuit/graphql/orchestrator/schema/transform/KeyTransformerPostMergeTest.java
@@ -0,0 +1,13 @@
+package com.intuit.graphql.orchestrator.schema.transform;
+
+public class KeyTransformerPostMergeTest {
+
+  public void transform_success() {
+
+  }
+
+  public void transform_notTheSameTypeDefinition_failed() {
+
+  }
+
+}
