3c65a1bba5f5d4d71deeb48365f65ad5bb60d136
Aureus, Carlo <chino.aureus@gmail.com>
2022-03-12 00:27:33 -0800

    refactoring JavaPrimitiveScalarSpec

--- src/test/groovy/com/intuit/graphql/orchestrator/integration/JavaPrimitiveScalarSpec.groovy
+++ src/test/groovy/com/intuit/graphql/orchestrator/integration/JavaPrimitiveScalarSpec.groovy
@@ -7,6 +7,8 @@ import graphql.ExecutionResult
 import graphql.execution.AsyncExecutionStrategy
 import spock.lang.Specification
 
+import java.util.concurrent.CompletableFuture
+
 import static com.intuit.graphql.orchestrator.GraphQLOrchestratorTest.createGraphQLOrchestrator
 
 class JavaPrimitiveScalarSpec extends Specification {
@@ -50,32 +52,28 @@ class JavaPrimitiveScalarSpec extends Specification {
     def specUnderTest = createGraphQLOrchestrator(new AsyncExecutionStrategy(),
             new AsyncExecutionStrategy(), testService)
 
+
     def "Extended Scalars for Primitive types are stitched and queried"() {
         given:
-        ExecutionInput query = ExecutionInput.newExecutionInput()
-                .query("{ a b c d e f g }")
+        def graphqlQuery = "{ a b c d e f g }"
+
+        ExecutionInput executionInput = ExecutionInput.newExecutionInput()
+                .query(graphqlQuery)
                 .build()
 
         when:
-        ExecutionResult executionResult = specUnderTest.execute(query).get()
+        CompletableFuture<ExecutionResult> futureExecutionResult = specUnderTest.execute(executionInput)
+        ExecutionResult executionResult = futureExecutionResult.get()
 
         then:
         executionResult.getErrors().isEmpty()
         Map<String, Object> data = executionResult.getData()
-        data.a instanceof Long
-        data.b instanceof Short
-        data.c instanceof Byte
-        data.d instanceof BigDecimal
-        data.e instanceof BigInteger
-        data.f instanceof Character
-        data.g instanceof Integer
-
-        data.a == Long.MAX_VALUE
-        data.b == Short.MAX_VALUE
-        data.c == Byte.MAX_VALUE
-        data.d == BigDecimal.valueOf(Double.MAX_VALUE)
-        data.e == new BigInteger("1234567890987654321")
-        data.f == Character.MAX_VALUE
-        data.g == Integer.MAX_VALUE
+        data.a instanceof Long && data.a == Long.MAX_VALUE
+        data.b instanceof Short && data.b == Short.MAX_VALUE
+        data.c instanceof Byte && data.c == Byte.MAX_VALUE
+        data.d instanceof BigDecimal && data.d == BigDecimal.valueOf(Double.MAX_VALUE)
+        data.e instanceof BigInteger && data.e == new BigInteger("1234567890987654321")
+        data.f instanceof Character && data.f == Character.MAX_VALUE
+        data.g instanceof Integer && data.g == Integer.MAX_VALUE
     }
 }
