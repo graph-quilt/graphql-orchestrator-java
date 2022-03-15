621356eabadc44d1e8d62c9f4118c6ff4637c77e
Aureus, Carlo <chino.aureus@gmail.com>
2022-03-15 02:45:42 -0700

    using ExtendedScalars constants

--- src/main/java/com/intuit/graphql/orchestrator/utils/ExtendedScalarsSupport.java
+++ src/main/java/com/intuit/graphql/orchestrator/utils/ExtendedScalarsSupport.java
@@ -1,6 +1,6 @@
 package com.intuit.graphql.orchestrator.utils;
 
-import graphql.Scalars;
+import graphql.scalars.ExtendedScalars;
 import graphql.schema.GraphQLScalarType;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,12 +10,12 @@ public class ExtendedScalarsSupport {
   public static final List<GraphQLScalarType> GRAPHQL_EXTENDED_SCALARS = new ArrayList<>();
 
   static {
-    GRAPHQL_EXTENDED_SCALARS.add(Scalars.GraphQLBigDecimal);
-    GRAPHQL_EXTENDED_SCALARS.add(Scalars.GraphQLBigInteger);
-    GRAPHQL_EXTENDED_SCALARS.add(Scalars.GraphQLByte);
-    GRAPHQL_EXTENDED_SCALARS.add(Scalars.GraphQLChar);
-    GRAPHQL_EXTENDED_SCALARS.add(Scalars.GraphQLShort);
-    GRAPHQL_EXTENDED_SCALARS.add(Scalars.GraphQLLong);
+    GRAPHQL_EXTENDED_SCALARS.add(ExtendedScalars.GraphQLBigDecimal);
+    GRAPHQL_EXTENDED_SCALARS.add(ExtendedScalars.GraphQLBigInteger);
+    GRAPHQL_EXTENDED_SCALARS.add(ExtendedScalars.GraphQLByte);
+    GRAPHQL_EXTENDED_SCALARS.add(ExtendedScalars.GraphQLChar);
+    GRAPHQL_EXTENDED_SCALARS.add(ExtendedScalars.GraphQLShort);
+    GRAPHQL_EXTENDED_SCALARS.add(ExtendedScalars.GraphQLLong);
   }
 
 }
