b4a011c5280621529d3885015e05936bb048884b
Aureus, Carlo <chino.aureus@gmail.com>
2022-03-16 00:27:42 -0700

    class to collect selections from a SelectionSet

---
+++ src/main/java/com/intuit/graphql/orchestrator/utils/SelectionCollector.java
@@ -0,0 +1,76 @@
+package com.intuit.graphql.orchestrator.utils;
+
+import graphql.language.Field;
+import graphql.language.FragmentDefinition;
+import graphql.language.FragmentSpread;
+import graphql.language.InlineFragment;
+import graphql.language.Selection;
+import graphql.language.SelectionSet;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import org.apache.commons.collections4.CollectionUtils;
+
+/**
+ * Collects set of fields from a selection set.  Fields collected may be from Inline Fragments
+ * or Fragment Spreads.
+ */
+public class SelectionCollector {
+
+  private final Map<String, FragmentDefinition> fragmentsByName;
+
+  public SelectionCollector(Map<String, FragmentDefinition> fragmentsByName) {
+    Objects.requireNonNull(fragmentsByName);
+    this.fragmentsByName = fragmentsByName;
+  }
+
+  public Map<String, Field> collectFields(SelectionSet selectionSet) {
+    if (selectionSet == null || CollectionUtils.isEmpty(selectionSet.getSelections())) {
+      return Collections.emptyMap();
+    }
+
+    // TODO test that a selection set has been deduped if same field
+    //  occurs in different selection (inline or fragment spread) on same level
+    return selectionSet.getSelections().stream()
+        .map(this::collectFields)
+        .flatMap(Collection::stream)
+        .collect(Collectors.toMap(Field::getName, Function.identity()));
+  }
+
+  private List<Field> collectFields(Selection selection) {
+    if (selection instanceof Field) {
+      return Collections.singletonList((Field) selection);
+    } else if (selection instanceof FragmentSpread) {
+      return getFields((FragmentSpread) selection);
+    } else if (selection instanceof InlineFragment) {
+      return getFields((InlineFragment) selection);
+    } else {
+      // the application is not in correct state.  Might consider updating.
+      throw new IllegalStateException(
+          "Unexpected selection instance.  class=" + selection.getClass().getName());
+    }
+  }
+
+  private List<Field> getFields(FragmentSpread fragmentSpread) {
+    Objects.requireNonNull(fragmentSpread);
+    FragmentDefinition fragmentDefinition = fragmentsByName.get(fragmentSpread.getName());
+    Objects.requireNonNull(fragmentDefinition);
+    return getFields(fragmentDefinition.getSelectionSet().getSelections());
+  }
+
+  private List<Field> getFields(InlineFragment inlineFragment) {
+    Objects.requireNonNull(inlineFragment);
+    return getFields(inlineFragment.getSelectionSet().getSelections());
+  }
+
+  private List<Field> getFields(List<Selection> selections) {
+    return selections.stream()
+        .flatMap(selection -> collectFields(selection).stream())
+        .collect(Collectors.toList());
+  }
+
+}
