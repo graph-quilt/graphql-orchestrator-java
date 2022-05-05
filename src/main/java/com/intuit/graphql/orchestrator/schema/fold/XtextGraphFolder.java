48c5defefaa078333d9755ddab7ca36349d151e6
kmoore15 <kyle_moore@intuit.com>
2022-05-05 13:31:32 -0400

    rename variable

--- src/main/java/com/intuit/graphql/orchestrator/schema/fold/XtextGraphFolder.java
+++ src/main/java/com/intuit/graphql/orchestrator/schema/fold/XtextGraphFolder.java
@@ -1,13 +1,5 @@
 package com.intuit.graphql.orchestrator.schema.fold;
 
-import static com.intuit.graphql.orchestrator.schema.fold.FieldMergeValidations.checkMergeEligibility;
-import static com.intuit.graphql.orchestrator.utils.DescriptionUtils.mergeDescriptions;
-import static com.intuit.graphql.orchestrator.utils.FederationConstants.FEDERATION_KEY_DIRECTIVE;
-import static com.intuit.graphql.orchestrator.utils.XtextUtils.definitionContainsDirective;
-import static com.intuit.graphql.orchestrator.xtext.DataFetcherContext.STATIC_DATAFETCHER_CONTEXT;
-import static com.intuit.graphql.orchestrator.xtext.GraphQLFactoryDelegate.createObjectType;
-import static com.intuit.graphql.utils.XtextTypeUtils.unwrapAll;
-
 import com.intuit.graphql.graphQL.EnumTypeDefinition;
 import com.intuit.graphql.graphQL.EnumValueDefinition;
 import com.intuit.graphql.graphQL.FieldDefinition;
@@ -22,13 +14,14 @@ import com.intuit.graphql.orchestrator.ServiceProvider;
 import com.intuit.graphql.orchestrator.schema.Operation;
 import com.intuit.graphql.orchestrator.schema.type.conflict.resolver.TypeConflictException;
 import com.intuit.graphql.orchestrator.schema.type.conflict.resolver.XtextTypeConflictResolver;
-import com.intuit.graphql.orchestrator.utils.FederationConstants;
-import com.intuit.graphql.orchestrator.utils.FederationUtils;
 import com.intuit.graphql.orchestrator.xtext.DataFetcherContext;
 import com.intuit.graphql.orchestrator.xtext.DataFetcherContext.DataFetcherType;
 import com.intuit.graphql.orchestrator.xtext.FieldContext;
 import com.intuit.graphql.orchestrator.xtext.XtextGraph;
 import com.intuit.graphql.utils.XtextTypeUtils;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+
 import java.util.Collection;
 import java.util.EnumMap;
 import java.util.HashMap;
@@ -37,8 +30,12 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.ecore.util.EcoreUtil;
+
+import static com.intuit.graphql.orchestrator.schema.fold.FieldMergeValidations.checkMergeEligibility;
+import static com.intuit.graphql.orchestrator.utils.DescriptionUtils.mergeDescriptions;
+import static com.intuit.graphql.orchestrator.xtext.DataFetcherContext.STATIC_DATAFETCHER_CONTEXT;
+import static com.intuit.graphql.orchestrator.xtext.GraphQLFactoryDelegate.createObjectType;
+import static com.intuit.graphql.utils.XtextTypeUtils.unwrapAll;
 
 public class XtextGraphFolder implements Foldable<XtextGraph> {
 
@@ -75,13 +72,13 @@ public class XtextGraphFolder implements Foldable<XtextGraph> {
     if(current.isFederationService()) {
       current.getValueTypesByName().values()
         .forEach(incomingSharedType -> {
-          TypeDefinition preexistingDefinition = accumulator.getType(incomingSharedType.getName());
+          TypeDefinition preexistingTypeDefinition = accumulator.getType(incomingSharedType.getName());
           if(incomingSharedType instanceof EnumTypeDefinition) {
-            mergeSharedValueType((EnumTypeDefinition) preexistingDefinition, (EnumTypeDefinition) incomingSharedType, current.getServiceProvider());
+            mergeSharedValueType((EnumTypeDefinition) preexistingTypeDefinition, (EnumTypeDefinition) incomingSharedType, current.getServiceProvider());
           } else if(incomingSharedType instanceof ObjectTypeDefinition) {
-            mergeSharedValueType((ObjectTypeDefinition) preexistingDefinition, (ObjectTypeDefinition) incomingSharedType, current.getServiceProvider());
+            mergeSharedValueType((ObjectTypeDefinition) preexistingTypeDefinition, (ObjectTypeDefinition) incomingSharedType, current.getServiceProvider());
           } else if(incomingSharedType instanceof InterfaceTypeDefinition) {
-            mergeSharedValueType((InterfaceTypeDefinition) preexistingDefinition, (InterfaceTypeDefinition) incomingSharedType, current.getServiceProvider());
+            mergeSharedValueType((InterfaceTypeDefinition) preexistingTypeDefinition, (InterfaceTypeDefinition) incomingSharedType, current.getServiceProvider());
           }
         });
     }
