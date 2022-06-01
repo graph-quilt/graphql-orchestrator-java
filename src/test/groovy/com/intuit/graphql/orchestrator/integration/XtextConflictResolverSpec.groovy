3ca054185282aaea7051beff094be44d72a2f179
Peter Chu <peter_chu@intuit.com>
2022-06-01 16:21:25 -0400

    DOS-1485: add integration test cases for Object, Field, Interface.

--- src/test/groovy/com/intuit/graphql/orchestrator/integration/XtextConflictResolverSpec.groovy
+++ src/test/groovy/com/intuit/graphql/orchestrator/integration/XtextConflictResolverSpec.groovy
@@ -1,10 +1,17 @@
 package com.intuit.graphql.orchestrator.integration
 
 import com.google.common.collect.ImmutableMap
+import com.intuit.graphql.orchestrator.ServiceProvider
+import com.intuit.graphql.orchestrator.TestHelper
 import com.intuit.graphql.orchestrator.TestServiceProvider
 import com.intuit.graphql.orchestrator.schema.Operation
+import com.intuit.graphql.orchestrator.testhelpers.SimpleMockServiceProvider
+import graphql.ExecutionInput
+import graphql.ExecutionResult
 import helpers.BaseIntegrationTestSpecification
 import spock.lang.Subject
+
+import static com.intuit.graphql.orchestrator.testhelpers.JsonTestUtils.jsonToMap
 
 
 class XtextConflictResolverSpec extends BaseIntegrationTestSpecification {
@@ -92,4 +99,234 @@ class XtextConflictResolverSpec extends BaseIntegrationTestSpecification {
         queryType.getFieldDefinition("bar") != null
         queryType.getFieldDefinition("foo").type == queryType.getFieldDefinition("bar").type
     }
+
+    def "Query Service A"() {
+        given:
+        SimpleMockServiceProvider serviceA = createMockService("SVC_A",
+            "top_level/rename/alpha-o-svc.graphqls",
+            "top_level/rename/mock-responses/get-alpha-o.json")
+        SimpleMockServiceProvider serviceB = createMockService("SVC_B",
+            "top_level/rename/beta-o-svc.graphqls",
+            "top_level/rename/mock-responses/get-beta-o.json")
+
+        specUnderTest = createGraphQLOrchestrator([serviceA, serviceB])
+
+        def graphqlQuery = """
+            query {
+              a { 
+                test
+                alpha
+              }
+            }
+        """
+
+        ExecutionInput executionInput = createExecutionInput(graphqlQuery)
+
+        when:
+        ExecutionResult executionResult = specUnderTest.execute(executionInput).get()
+
+        then:
+        executionResult.getErrors().isEmpty()
+
+        specUnderTest.runtimeGraph?.getType("MyType1") != null
+        specUnderTest.runtimeGraph?.getType("beta") != null
+
+        Map<String, Object> data = executionResult.getData()
+        data.a?.test instanceof String && data.a?.test == "1..2..3"
+        data.a?.alpha instanceof String && data.a?.alpha == "Alpha"
+
+    }
+
+    def "Query Service B With Renamed Object"() {
+        given:
+        SimpleMockServiceProvider serviceA = createMockService("SVC_A",
+            "top_level/rename/alpha-o-svc.graphqls",
+            "top_level/rename/mock-responses/get-alpha-o.json")
+        SimpleMockServiceProvider serviceB = createMockService("SVC_B",
+            "top_level/rename/beta-o-svc.graphqls",
+            "top_level/rename/mock-responses/get-beta-o.json")
+
+        specUnderTest = createGraphQLOrchestrator([serviceA, serviceB])
+
+        def graphqlQuery = """
+            query {
+              b { 
+                test
+                beta
+              }
+            }
+        """
+
+        ExecutionInput executionInput = createExecutionInput(graphqlQuery)
+
+        when:
+        ExecutionResult executionResult = specUnderTest.execute(executionInput).get()
+
+        then:
+        executionResult.getErrors().isEmpty()
+
+        specUnderTest.runtimeGraph?.getType("MyType1") != null
+        specUnderTest.runtimeGraph?.getType("beta") != null
+
+        Map<String, Object> data = executionResult.getData()
+        data.b?.test instanceof String && data.b?.test == "One..Two..Three"
+        data.b?.beta instanceof String && data.b?.beta == "Beta"
+    }
+
+    def "Query Service A With Field"() {
+        given:
+        SimpleMockServiceProvider serviceA = createMockService("SVC_A",
+            "top_level/rename/alpha-f-svc.graphqls",
+            "top_level/rename/mock-responses/get-alpha-f.json")
+        SimpleMockServiceProvider serviceB = createMockService("SVC_B",
+            "top_level/rename/beta-f-svc.graphqls",
+            "top_level/rename/mock-responses/get-beta-f.json")
+
+        specUnderTest = createGraphQLOrchestrator([serviceA, serviceB])
+
+        def graphqlQuery = """
+            query {
+              a { 
+                test
+                alpha
+              }
+            }
+        """
+
+        ExecutionInput executionInput = createExecutionInput(graphqlQuery)
+
+        when:
+        ExecutionResult executionResult = specUnderTest.execute(executionInput).get()
+
+        then:
+        executionResult.getErrors().isEmpty()
+
+        specUnderTest.runtimeGraph?.getExecutableSchema().getType("Query").getFieldDefinition("b") == null
+        specUnderTest.runtimeGraph?.getExecutableSchema().getType("Query").getFieldDefinition("bb") != null
+        specUnderTest.runtimeGraph?.getType("MyTypeB") != null
+
+        Map<String, Object> data = executionResult.getData()
+        data.a?.test instanceof String && data.a?.test == "1..2..3"
+        data.a?.alpha instanceof String && data.a?.alpha == "Alpha"
+    }
+
+    def "Query Service B With Renamed Field"() {
+        given:
+        SimpleMockServiceProvider serviceA = createMockService("SVC_A",
+            "top_level/rename/alpha-f-svc.graphqls",
+            "top_level/rename/mock-responses/get-alpha-f.json")
+        SimpleMockServiceProvider serviceB = createMockService("SVC_B",
+            "top_level/rename/beta-f-svc.graphqls",
+            "top_level/rename/mock-responses/get-beta-f.json")
+
+        specUnderTest = createGraphQLOrchestrator([serviceA, serviceB])
+
+        def graphqlQuery = """
+            query {
+              bb {
+                test
+                beta
+              }
+            }
+        """
+
+        ExecutionInput executionInput = createExecutionInput(graphqlQuery)
+
+        when:
+        ExecutionResult executionResult = specUnderTest.execute(executionInput).get()
+
+        then:
+        executionResult.getErrors().isEmpty()
+
+        specUnderTest.runtimeGraph?.getExecutableSchema().getType("Query").getFieldDefinition("b") == null
+        specUnderTest.runtimeGraph?.getExecutableSchema().getType("Query").getFieldDefinition("bb") != null
+        specUnderTest.runtimeGraph?.getType("MyTypeB") != null
+
+        Map<String, Object> data = executionResult.getData()
+        data.bb?.test instanceof String && data.bb?.test == "One..Two..Three"
+        data.bb?.beta instanceof String && data.bb?.beta == "Beta"
+    }
+
+    def "Query Service I With Interface"() {
+        given:
+        SimpleMockServiceProvider serviceA = createMockService("SVC_A",
+                "top_level/rename/alpha-i-svc.graphqls",
+                "top_level/rename/mock-responses/get-alpha-i.json")
+        SimpleMockServiceProvider serviceB = createMockService("SVC_B",
+                "top_level/rename/beta-i-svc.graphqls",
+                "top_level/rename/mock-responses/get-beta-i.json")
+
+        specUnderTest = createGraphQLOrchestrator([serviceA, serviceB])
+
+        def graphqlQuery = """
+            fragment NameFragment on IName {
+                name
+            }
+            query {
+                a {
+                    ...NameFragment
+                    test
+                    alpha
+                }
+            }
+        """
+
+        ExecutionInput executionInput = createExecutionInput(graphqlQuery)
+
+        when:
+        ExecutionResult executionResult = specUnderTest.execute(executionInput).get()
+
+        then:
+        executionResult.getErrors().isEmpty()
+        Map<String, Object> data = executionResult.getData()
+        data.a?.name instanceof String && data.a?.name == "A A"
+        data.a?.test instanceof String && data.a?.test == "1..2..3"
+        data.a?.alpha instanceof String && data.a?.alpha == "Alpha"
+    }
+
+    def "Query Service B With Renamed Interface"() {
+        given:
+        SimpleMockServiceProvider serviceA = createMockService("SVC_A",
+            "top_level/rename/alpha-i-svc.graphqls",
+            "top_level/rename/mock-responses/get-alpha-i.json")
+        SimpleMockServiceProvider serviceB = createMockService("SVC_B",
+            "top_level/rename/beta-i-svc.graphqls",
+            "top_level/rename/mock-responses/get-beta-i.json")
+
+        specUnderTest = createGraphQLOrchestrator([serviceA, serviceB])
+
+        def graphqlQuery = """
+            fragment NameFragment on ITheName {
+                name
+            }
+            query {
+                b {
+                    ...NameFragment
+                    test
+                    beta
+                }
+            }
+        """
+
+        ExecutionInput executionInput = createExecutionInput(graphqlQuery)
+
+        when:
+        ExecutionResult executionResult = specUnderTest.execute(executionInput).get()
+
+        then:
+        executionResult.getErrors().isEmpty()
+        Map<String, Object> data = executionResult.getData()
+        data.b?.name instanceof String && data.b?.name == "B B"
+        data.b?.test instanceof String && data.b?.test == "One..Two..Three"
+        data.b?.beta instanceof String && data.b?.beta == "Beta"
+    }
+
+    SimpleMockServiceProvider createMockService(String namespace, String schemaFile, String responseFile) {
+        return (SimpleMockServiceProvider)SimpleMockServiceProvider.builder()
+            .namespace(namespace)
+            .sdlFiles(TestHelper.getFileMapFromList(schemaFile))
+            .mockResponse(jsonToMap(TestHelper.getResourceAsString(responseFile)))
+            .build()
+    }
+
 }
