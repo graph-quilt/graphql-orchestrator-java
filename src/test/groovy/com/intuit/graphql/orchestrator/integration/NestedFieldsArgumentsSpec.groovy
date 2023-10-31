87a30eab9214e75f4a49f92f61bbdbdf17129991
tasneemkoushar <imatasneemkoushar@gmail.com>
2023-10-31 17:02:18 +0530

    fix: made changes for service A, B, C, D, E

--- src/test/groovy/com/intuit/graphql/orchestrator/integration/NestedFieldsArgumentsSpec.groovy
+++ src/test/groovy/com/intuit/graphql/orchestrator/integration/NestedFieldsArgumentsSpec.groovy
@@ -1,17 +1,35 @@
 package com.intuit.graphql.orchestrator.integration
 
 import com.intuit.graphql.orchestrator.GraphQLOrchestrator
-import com.intuit.graphql.orchestrator.ServiceA
-import com.intuit.graphql.orchestrator.ServiceB
 import com.intuit.graphql.orchestrator.ServiceProvider
 import com.intuit.graphql.orchestrator.schema.transform.FieldMergeException
 import helpers.BaseIntegrationTestSpecification
 
 class NestedFieldsArgumentsSpec extends BaseIntegrationTestSpecification {
+    def mockAServiceResponse = [
+            data: [
+                    serviceA: []
+            ]
+    ]
+
+    def mockBServiceResponse = [
+            data: [
+                    serviceB: []
+            ]
+    ]
 
     def "cannot Build Due To Query Nested Fields Has Mismatched Arguments"() {
+        def serviceA = createSimpleMockService( "SVCA", "type Query { container : Container } "
+                + "type Container { serviceA : ServiceA } "
+                + "type ServiceA { svcAField1 : String }", mockAServiceResponse)
+
+        def serviceB = createSimpleMockService( "SVCB", "type Query { container(in : String) : Container } "
+                + "type Container { serviceB : ServiceB } "
+                + "type ServiceB { svcBField1 : String }", mockBServiceResponse)
+
+
         when:
-        ServiceProvider[] services = [ new ServiceA(), new ServiceB() ]
+        ServiceProvider[] services = [ serviceA, serviceB ]
         GraphQLOrchestrator orchestrator = createGraphQLOrchestrator(services)
 
         then:
