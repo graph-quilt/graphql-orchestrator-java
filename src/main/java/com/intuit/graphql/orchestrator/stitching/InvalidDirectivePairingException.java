73dba531e149d5324471a7586093b5cbaff3c8e5
vkarthikeya <vijaya_karthikeyan@intuit.com>
2023-10-31 16:49:01 -0700

    Hactoberfest2023 : Multiple Directive Coexist validation

--- src/main/java/com/intuit/graphql/orchestrator/stitching/InvalidDirectivePairingException.java
+++ src/main/java/com/intuit/graphql/orchestrator/stitching/InvalidDirectivePairingException.java
@@ -1,6 +1,7 @@
-package com.intuit.graphql.orchestrator.resolverdirective;
+package com.intuit.graphql.orchestrator.stitching;
 
-public class InvalidDirectivePairingException extends ResolverDirectiveException {
+
+public class InvalidDirectivePairingException extends StitchingException {
 
   private static final String ERR_MSG = "Field %s in container type %s with resolver directive not allowed "
       + "to have argument definitions.";
